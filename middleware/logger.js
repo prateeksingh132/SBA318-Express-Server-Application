8514811553279013069615ed88ed2669de50f7e0
prateeksingh132 <kumarpks132@gmail.com>
2026-01-25 16:07:31 -0700

    Create a basic logger logreq function  for now, will refine it later.

--- middleware/logger.js
+++ middleware/logger.js
@@ -4,3 +4,12 @@
 // similar to the warmup exercise we did in class.
 
 
+export function logReq(req, res, next) {
+    // i want to see the method and the url in my terminal so i know what is happening
+    console.log(`[Request]: ${req.method} ${req.url}`);
+
+    // FUTUREWORK: add log for POST later
+
+    // Important: must call next() otherwise the app gets stuck here and browser keeps spinning
+    next();
+}
