a3257030319fefe6e3be6e79fa86eb32262e54c7
prateeksingh132 <kumarpks132@gmail.com>
2026-01-25 15:39:05 -0700

    Adding logic as i go along. Starting with creating mock database..simulating using arrays...

--- database/data.js
+++ database/data.js
@@ -1,3 +1,6 @@
 //////// Mock Database
 
 
+// Goal: since we did not learn database yet, i am simulating a database here using arrays.
+// i am exporting these arrays so i can import them in my server or controller.
+// this satisfies the requirement of "Use at least three different data categories".
