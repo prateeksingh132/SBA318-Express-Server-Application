b96483aa193d22337ff75f57a9daa8d043362d9a
prateeksingh132 <kumarpks132@gmail.com>
2026-01-25 19:20:33 -0700

    Working on my product controller  now. Created five functions for GET, POST, DELETE, PUT/PATCH, and also one to show my form later on on view. will work on the logic now.

--- controller/productController.js
+++ controller/productController.js
@@ -1,1 +1,32 @@
 //////// GadgetShack product controller
+
+// Goal: this file handles the logic for product routes.
+// i separated this from the routes file to keep my code organized (modularization).
+// this will help in later as well during capstone when logic gets complicated.
+
+import { products } from "../database/data.js";
+
+// get all products (with filtering)
+export const getAllProducts = (req, res) => {
+    
+};
+
+// create new product
+export const createProduct = (req, res, next) => {
+    
+};
+
+// delete product
+export const deleteProduct = (req, res) => {
+    
+};
+
+// show edit form
+export const showEditForm = (req, res) => {
+    
+};
+
+// update product (patch)
+export const updateProduct = (req, res) => {
+    
+};
