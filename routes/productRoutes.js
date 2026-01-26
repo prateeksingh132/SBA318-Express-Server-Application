5888144672d5e162ca9f07af7c84d7d9f21362f9
prateeksingh132 <kumarpks132@gmail.com>
2026-01-25 19:33:28 -0700

    Created the skeleton for my route script, added route definition. Will work on controller logic now.

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,1 +1,24 @@
 /////// API logic: All routes logic for gadgetshack product page
+
+import express from "express";
+const router = express.Router();
+
+// importing the controller logic
+// keeping my routes clean...
+import {
+    getAllProducts,
+    createProduct,
+    deleteProduct,
+    showEditForm,
+    updateProduct
+} from "../controller/productController.js";
+
+
+// route definitions
+router.get("/", getAllProducts);           // view all products
+router.post("/", createProduct);           // create new product
+router.get("/:id/edit", showEditForm);     // show edit form
+router.patch("/:id", updateProduct);       // update product
+router.delete("/:id", deleteProduct);      // delete product
+
+export default router;
