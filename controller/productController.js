19068e714a722d75f742a7a9af6bce211483abd7
prateeksingh132 <kumarpks132@gmail.com>
2026-01-27 00:48:47 -0700

    Created logic for updateproduct, divided it into 2 parts, edit/show form and then actual update. Tested, works well so far. found 2 bugs: like if partial update done, server crashed. i rectified by adding checks. will work on view styling and refining the code now...

--- controller/productController.js
+++ controller/productController.js
@@ -45,6 +45,8 @@ export const getAllProducts = (req, res) => {
                     <button type="submit" class="delete-btn">Delete</button>
                 </form>
 
+                <a href="/products/${p.id}/edit">Edit</a>
+
             </div>
         `;
     }
@@ -104,6 +106,7 @@ export const createProduct = (req, res, next) => {
 // the issue is that to delete an item from an array, i first need to find its index (position).
 // i cannot delete directly by id, so i use findindex() to get the position, then splice() to remove it.
 // i referred this sample example on stackoverflow that did the same: - https://stackoverflow.com/questions/15287865/remove-array-element-based-on-object-property
+// also dont forget about this link which i put on server.js which gave an example of using method-override: https://stackoverflow.com/questions/72611507/form-delete-method-is-redirecting-to-the-get-method-instead-in-express-js
 export const deleteProduct = (req, res) => {
 
     // getting the id from the url params
@@ -117,7 +120,7 @@ export const deleteProduct = (req, res) => {
         products.splice(index, 1); // remove 1 item at that index
 
         ////////////TESTING
-        // console.log(`Deleted product with id: ${id}`);
+        // console.log(`TESTING: Deleted product with id: ${id}`);
         ////////////TESTING
     }
 
@@ -126,12 +129,74 @@ export const deleteProduct = (req, res) => {
 
 };
 
+
+
+/////////// Update/Patch 
+// Goal: so the idea is that the i am gonna create an edit button in each product card. when i clicks edit,the server has to find the item and it shows a form filled with the old data
+// and then i can update and when i clicks updtae button, the server has to find the item again, overwrite the old data with new data, and save it
+// thas why i have divided the whole logic of update/patch into two parts: show edit form, update the product
+
 // show edit form
+// Goal: find the product by id and then show a form filled with its data
 export const showEditForm = (req, res) => {
+
+    const id = parseInt(req.params.id);
+    const product = products.find(p => p.id === id);
+
+    // if wrong id is entered
+    if (!product) return res.send("Product not found");
+
+    // creating a specific form just for editing
+    // Important: HTML forms dont support patch, so i m gonna use the ?_method=PATCH again just like in delete controller using method-override middleware
+    // so when we click submit, it sends the data to /products/productId and kind of simulates a patch request.
+    // Note: i tested and noticed that the form initially is empty, so i dont know what to change in it. thats why, i needed to populate the input with exisiting data, else its confusing, so used value = ""
+    let editForm = `
+        <div class="form-container">
+            <h2>Edit ${product.name}</h2>
+            <form action="/products/${product.id}?_method=PATCH" method="POST">
+                <label>Name:</label>
+                <input type="text" name="name" value="${product.name}" />
+                <br>
+                <label>Price:</label>
+                <input type="number" name="price" value="${product.price}" />
+                <br>
+                <button type="submit">Update Item</button>
+            </form>
+            <a href="/products">Cancel</a>
+        </div>
+    `;
+
+    res.render("index", { title: "Edit Product", content: editForm });
+
 
 };
 
-// update product (patch)
+// 2. update product
+// Goal: actually update the data in our array
+// i tested and i noticed that once the showeditform finishes, server forgot everything, its kind of a brand new event, thats why i have to search product again, look at the url and parse the id and then search the array to find the product object, all over again.
+// once i find the product object from the array, then i change the updated properties
 export const updateProduct = (req, res) => {
+
+    const id = parseInt(req.params.id);
+    const product = products.find(p => p.id === id);
+
+
+    // i tested and the server shows error if i enter a wrong id, so i need to put a check here. make sense bcuz we cant send an update request for product that doesnt exist.
+    if (product) {
+        // so if i only update one property, it kind of deletes the other prperty and put undefined in it. 
+        // thats why i need to add check here in case one of the properties were not updated, i update only propteries that were changed and sent
+        // so, only update properties if they were sent in the body
+        if (req.body.name) product.name = req.body.name;
+        if (req.body.price) product.price = req.body.price;
+
+        ////////////TESTING
+        // console.log(`TESTING: Updated product ${id} with new data..`);
+        ////////////
+
+    }
+
+    // after updating, go back to the list
+    res.redirect("/products");
+
 
 };
