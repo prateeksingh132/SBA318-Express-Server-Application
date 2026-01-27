3c9456129891b47bd11967b6a7d0983bd9ee246d
prateeksingh132 <kumarpks132@gmail.com>
2026-01-27 15:38:28 -0700

    Added dayjs package for timestamp logging, to satisfy bonus requirment. Tested, works well so far...

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,12 +1,24 @@
 /////// Global logging middleware
 
+import dayjs from 'dayjs';
+
 // Goal: create a middleware to log requests.
 // similar to the warmup exercise we did in class.
 
 
+////// Bonus Objectives
+// Requirement: Research and use a third-party package.
+// i found dayjs is great for formatting dates.
+// https://www.npmjs.com/package/dayjs
+
+
 export function logReq(req, res, next) {
+
+    // using dayjs to format the current time nicely
+    const currentTime = dayjs().format('YYYY-MM-DD HH:mm:ss');
+
     // i want to see the method and the url in my terminal so i know what is happening
-    console.log(`[Request]: ${req.method} ${req.url}`);
+    console.log(`[${currentTime}] [Request]: ${req.method} ${req.url}`);
 
     // FUTUREWORK: add log for POST later
 
